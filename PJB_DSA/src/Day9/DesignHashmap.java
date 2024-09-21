8cf95f7632c5abdc307ad3a5a4d98929861e7e24
C4tTu0n9 <tuongpche171654@fpt.edu.vn>
2024-09-21 08:38:06 +0700

    merge DesignHashMap day 9

--- PJB_DSA/src/Day9/DesignHashmap.java
+++ PJB_DSA/src/Day9/DesignHashmap.java
@@ -1,21 +1,68 @@
 package Day9;
 
+import java.util.ArrayList;
+
 public class DesignHashmap {
 
+    ArrayList<Integer> keys = new ArrayList<>();
+    ArrayList<Integer> values = new ArrayList<>();
     public DesignHashmap() {
 
     }
 
     public void put(int key, int value) {
-
+        if(keys.isEmpty()){
+            keys.add(key);
+            values.add(value);
+            return;
+        }
+        for (int i = 0; i < keys.size(); i++) {
+            if(keys.get(i) == key){
+                values.set(i, value);
+                return;
+            }
+        }
+        keys.add(key);
+        values.add(value);
     }
 
     public int get(int key) {
-
+        for (int i = 0; i < keys.size(); i++) {
+            if(keys.get(i) == key){
+                return values.get(i);
+            }
+        }
+        return -1;
     }
 
     public void remove(int key) {
+        for (int i = 0; i < keys.size(); i++) {
+            if(keys.get(i) == key){
+                keys.remove(i);
+                values.remove(i);
+                break;
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+        DesignHashmap dhm = new DesignHashmap();
+        dhm.put(1, 1);
+        dhm.put(2, 12);
+        dhm.put(3, 123);
+        dhm.put(4, 5);
+        dhm.put(1, 10);
+        for(int i : dhm.values){
+            System.out.print(i+" -> ");
+        }
+        System.out.println();
+        dhm.remove(1);
+        for(int i : dhm.values){
+            System.out.print(i+" -> ");
+        }
+        System.out.println();
+        System.out.println(dhm.get(4));
+        System.out.println(dhm.get(10));
     }
 }
 
